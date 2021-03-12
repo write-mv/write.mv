abcd03b38b3b8559ca98d6bad6b94e499be9915b
Mohamed jinas <j@live.mv>
2021-03-13 01:36:26 +0500

    color tweak

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,5 +1,5 @@
 <x-guest-layout>
-    <div class="min-h-screen bg-gray-50 flex flex-col justify-center py-12 sm:px-6 lg:px-8">
+    <div class="min-h-screen bg-gray-100 flex flex-col justify-center py-12 sm:px-6 lg:px-8">
         <div class="sm:mx-auto sm:w-full sm:max-w-md">
           <img class="mx-auto h-16 w-auto" src="/images/logo.svg">
           <h2 class="mt-6 text-center text-3xl font-extrabold text-gray-900">
