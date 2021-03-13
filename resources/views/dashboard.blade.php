80689f6ec265bc87ab87f6e30cf39db784e2c1fd
Mohamed jinas <j@live.mv>
2021-03-13 05:27:32 +0500

    custom 404

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -29,7 +29,8 @@
                     <div class="mt-5">
                         <a
                             href="{{route('posts.new')}}"
-                            class="inline-flex items-center justify-center px-4 py-2 mr-2 border border-transparent font-medium rounded-md text-blue-700 bg-blue-100 hover:bg-blue-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 sm:text-sm">
+                            class="mr-2 inline-flex justify-center items-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
+                            <x-heroicon-o-newspaper class="w-5 h-5 mr-1" />
                             New Post
                         </a>
 
