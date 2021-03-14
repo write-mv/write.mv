2d6f733d94ca6493e8bbcf62fd32180a5d130df5
Mohamed jinas <j@live.mv>
2021-03-14 05:32:18 +0500

    error page

---
+++ resources/views/errors/503.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Write.mv - Rant, share & scribble</title>
+    <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="{{ asset('css/font.css') }}">
+</head>
+
+<body>
+    <section class="container px-4 py-32 mx-auto">
+        <div class="w-full mx-auto lg:w-1/3">
+          <a href="/" title="Hellonext Home Page" class="flex items-center">
+            <img class="w-auto h-28" src="/images/logo.svg">
+          </a>
+          <p class="mt-5 mb-3 text-xl font-bold text-black md:text-2xl">
+           Service Unavailable (503)
+          </p>
+          <p class="mb-3 text-base font-medium text-gray-700">
+           Be right back! We are making some changes to the website
+          </p>
+        </div>
+      </section>
+      
+</body>
+
+</html>
