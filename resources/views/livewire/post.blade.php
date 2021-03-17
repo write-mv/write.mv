aa376b6fcd25d2e1bb90b3c9a811695d5ce883d3
Mohamed Jinas <j@live.mv>
2021-03-17 09:49:47 +0500

    Update post.blade.php

--- resources/views/livewire/post.blade.php
+++ resources/views/livewire/post.blade.php
@@ -16,11 +16,6 @@
                   <x-form.toggle model="post.is_english" />
 
                 </div>
-                <button type="submit"
-                  class="ml-3 inline-flex justify-center items-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-gray-600 bg-white hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 poppins">
-                  <x-heroicon-o-eye class="text-gray-600 h-5 w-5 mr-2" />
-                  <span>Preview</span>
-                </button>
                 <button type="submit" wire:loading.attr="disabled"
                   class="ml-3 inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 poppins">
                   <div wire:loading wire:target="save">
