0a55bc1bc2d771fa57f2e8abc646a3265f13e3f2
Mohamed Jinas <j@live.mv>
2021-03-17 05:23:30 +0500

    modal font change

--- resources/views/components/modal/dialog.blade.php
+++ resources/views/components/modal/dialog.blade.php
@@ -1,7 +1,7 @@
 @props(['id' => null, 'maxWidth' => null])
 
 <x-modal :id="$id" :maxWidth="$maxWidth" {{ $attributes }}>
-    <div class="px-6 py-4">
+    <div class="px-6 py-4 poppins">
         <div class="text-lg">
             {{ $title }}
         </div>
