cc22a8fadfda1c80e2b9d715778550ae5488f0e4
Mohamed Jinas <j@live.mv>
2021-03-17 05:36:48 +0500

    update

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -2,7 +2,7 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
 
-            <div class="bg-white sm:rounded-lg mb-5 w-1/2">
+            <div class="bg-white sm:rounded-lg mb-5 lg:w-1/2">
                 <div class="px-4 py-5 sm:p-6">
                     <h3 class="text-xl font-semibold text-gray-700 mb-5 poppins">
                         Blog
@@ -18,18 +18,18 @@
                     <div class="mt-5">
                         <a
                             href="{{route('posts.new')}}"
-                            class="mr-2 inline-flex justify-center items-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 sm:text-sm poppins">
+                            class="mr-2 inline-flex justify-center mt-3 items-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 sm:text-sm poppins">
                             New Post
                         </a>
 
                         <a
                             href="{{route('blog.customize', $blog)}}"
-                            class="inline-flex items-center justify-center px-4 py-2 mr-2 border border-transparent font-medium rounded-md text-gray-700 bg-gray-100 hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500 sm:text-sm poppins">
+                            class="inline-flex items-center mt-3 justify-center px-4 py-2 mr-2 border border-transparent font-medium rounded-md text-gray-700 bg-gray-100 hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500 sm:text-sm poppins">
                             Customize
                         </a>
                         <a
                             href="{{$blog->url}}"
-                            class="inline-flex items-center justify-center px-4 py-2 border border-transparent font-medium rounded-md text-gray-700 bg-gray-100 hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500 sm:text-sm poppins">
+                            class="inline-flex items-center mt-3 justify-center px-4 py-2 border border-transparent font-medium rounded-md text-gray-700 bg-gray-100 hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500 sm:text-sm poppins">
                             View Blog
                             <x-heroicon-o-arrow-narrow-right class="w-3 h-3 ml-1 text-gray-500" />
                         </a>
