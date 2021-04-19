651a08c895e95a86f4a341e5fdeb87625aba67ef
Mohamed jinas <j@live.mv>
2021-04-19 23:53:29 +0500

    Automatically generate slug for the article

--- app/Http/Livewire/Post.php
+++ app/Http/Livewire/Post.php
@@ -10,6 +10,7 @@ use Illuminate\Support\Str;
 use Livewire\WithFileUploads;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Validation\Rule;
+use App\Modules\ThaanaTransliterator;
 
 class Post extends Component
 {
@@ -34,6 +35,22 @@ class Post extends Component
             $this->post = $post;
         } else {
             $this->post = $this->makeBlankPost();
+        }
+    }
+    
+    /**
+     * Automatically generating the slug and filling the input
+     *
+     * @param $title $title [explicite description]
+     *
+     * @return void
+     */
+    public function updatingPostTitle($title)
+    {
+        if($this->post->is_english) {
+            $this->post->slug = Str::slug($title);
+        }else {
+            $this->post->slug = Str::slug(ThaanaTransliterator::transliterate($title));
         }
     }
 
