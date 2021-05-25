2b6a18699558269f37a3912ff69814452ea79a96
Mohamed jinas <j@live.mv>
2021-05-25 15:24:30 +0500

    Seo

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,16 +4,16 @@ namespace App\Http\Controllers;
 
 use App\Models\Blog;
 use Illuminate\Http\Request;
+use Artesaos\SEOTools\Facades\SEOTools;
 
 class PostController extends Controller
 {
     public function index($name)
     {
-        $blog = Blog::withoutGlobalScopes()->with('theme')->where('name', $name)->first();
-
-        if (!$blog) {
-            abort(404);
-        }
+        $blog = Blog::withoutGlobalScopes()
+            ->with('theme')
+            ->where('name', $name)
+            ->firstOrFail();
 
         $blog->RecordView();
 
@@ -26,6 +26,8 @@ class PostController extends Controller
             $themeDir =  "themes::default._list";
         }
 
+        $this->buildBlogSeo();
+
         return view($themeDir, [
             'blog' => $blog,
             'posts' => $posts
@@ -34,11 +36,10 @@ class PostController extends Controller
 
     public function show($name, $post)
     {
-        $blog = Blog::withoutGlobalScopes()->with('theme')->where('name', $name)->first();
-
-        if (!$blog) {
-            abort(404);
-        }
+        $blog = Blog::withoutGlobalScopes()
+            ->with('theme')
+            ->where('name', $name)
+            ->firstOrFail();
 
         $post = $blog->posts()->withoutGlobalScopes()->live()->where('slug', $post)->first();
 
@@ -65,5 +66,16 @@ class PostController extends Controller
             'post' => $post,
             //'comments' => $comments
         ]);
+    }
+
+    protected function buildBlogSeo()
+    {
+        SEOTools::setTitle('Home');
+        SEOTools::setDescription('This is my page description');
+        SEOTools::opengraph()->setUrl('http://current.url.com');
+        SEOTools::setCanonical('https://codecasts.com.br/lesson');
+        SEOTools::opengraph()->addProperty('type', 'articles');
+        SEOTools::twitter()->setSite('@LuizVinicius73');
+        SEOTools::jsonLd()->addImage('https://codecasts.com.br/img/logo.jpg');
     }
 }
