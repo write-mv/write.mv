ca0b14893e589f555c764d1302d159c871605098
Mohamed jinas <j@live.mv>
2021-07-28 09:27:11 +0500

    update

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -2,7 +2,7 @@
 <td class="header">
 <a href="{{ $url }}" style="display: inline-block;">
 @if (trim($slot) === 'Laravel')
-<img src="/images/logo.svg" class="logo" alt="Writemv Logo">
+<img src="https://write.mv/images/logo.png" class="logo" alt="Laravel Logo">
 @else
 {{ $slot }}
 @endif
