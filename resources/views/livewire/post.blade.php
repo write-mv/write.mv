736d51fde3123702561614f74f31d41ffccb6767
Mohamed jinas <j@live.mv>
2021-07-28 07:42:24 +0500

    Update post.blade.php

--- resources/views/livewire/post.blade.php
+++ resources/views/livewire/post.blade.php
@@ -166,6 +166,7 @@
                   <div class="col-span-6 sm:col-span-2 mt-3">
                     <label for="tags" class="block text-sm font-medium text-gray-700 dark:text-gray-200 poppins mb-2">Tags</label>
                     <x-multiselect
+                    trackBy="id"
                     placeholder="Select a tag"
                     wire:model="tags"
                     :options="$AvailableTags"
