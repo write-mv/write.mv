2a94bde7e0b75c9b76928eede561523fb41d0fd8
Mohamed jinas <j@live.mv>
2021-08-18 19:39:26 +0500

    Update list-posts.blade.php

--- resources/views/livewire/list-posts.blade.php
+++ resources/views/livewire/list-posts.blade.php
@@ -145,7 +145,7 @@
                                             <span>View stats</span>
                                         </a>
                                         @if ($post->isPublished())
-                                            <a href="{{ route('posts.show', ['name' => $post->blog->name, 'post' => $post->slug]) }}"
+                                            <a href="{{ route('domain.posts.show', ['name' => $post->blog->name, 'post' => $post->slug]) }}"
                                                 class="flex items-center px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900 poppins"
                                                 role="menuitem">
                                                 <svg class="w-6 h-6 mr-2" fill="none" stroke="currentColor"
