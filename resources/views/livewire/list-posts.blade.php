ca65effdbf3370fc91607fb605ca5497d01e8a07
Mohamed jinas <j@live.mv>
2021-08-28 02:20:57 +0500

    Update list-posts.blade.php

--- resources/views/livewire/list-posts.blade.php
+++ resources/views/livewire/list-posts.blade.php
@@ -185,6 +185,7 @@
                                                 </svg>
                                                 <span>Edit post</span>
                                             </a>
+
                                             <a href="#" wire:click="openDeleteModal({{ $post->id }})"
                                                 class="px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900 poppins flex items-center"
                                                 role="menuitem">
@@ -197,57 +198,20 @@
                                                 </svg>
                                                 <span>Delete post</span>
                                             </a>
-                                            <a href="{{ route('stats.show', $post->id) }}"
-                                        @endif
-                                        <a href="{{ route('posts.update', $post) }}"
-                                            class="px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900 poppins flex items-center"
-                                            role="menuitem">
-                                            <svg class="w-6 h-6 mr-2" fill="none" stroke="currentColor"
-                                                viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                                    d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z">
-                                                </path>
-                                            </svg>
-                                            <span>Edit post</span>
-                                        </a>
-                                        <a href="#" wire:click="openDeleteModal({{ $post->id }})"
-                                            class="px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900 poppins flex items-center"
-                                            role="menuitem">
-                                            <svg class="w-6 h-6 mr-2" fill="none" stroke="currentColor"
-                                                viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                                    d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16">
-                                                </path>
-                                            </svg>
-                                            <span>Delete post</span>
-                                        </a>
-                                        @if ($post->isPublished())
-                                            <a href="{{ route('stats.show', $post->id) }}"
-                                                class="flex items-center px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900 poppins"
-                                                role="menuitem">
-                                                <svg class="w-6 h-6 mr-2" fill="none" stroke="currentColor"
-                                                    viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                                                    <path stroke-linecap="round" stroke-linejoin="round"
-                                                        stroke-width="2"
-                                                        d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z">
-                                                    </path>
-                                                </svg>
-                                                <span>View stats</span>
-                                            </a>
-                                        @endif
-                                        @if ($post->isPublished())
-                                            <a href="{{ route('domain.posts.show', ['name' => $post->blog->name, 'post' => $post->slug]) }}"
-                                                class="flex items-center px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900 poppins"
-                                                role="menuitem">
-                                                <svg class="w-6 h-6 mr-2" fill="none" stroke="currentColor"
-                                                    viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                                                    <path stroke-linecap="round" stroke-linejoin="round"
-                                                        stroke-width="2"
-                                                        d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z">
-                                                    </path>
-                                                </svg>
-                                                <span>View stats</span>
-                                            </a>
+                                            @if ($post->isPublished())
+                                                <a href="{{ route('domain.posts.show', ['name' => $post->blog->name, 'post' => $post->slug]) }}"
+                                                    class="flex items-center px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900 poppins"
+                                                    role="menuitem">
+                                                    <svg class="w-6 h-6 mr-2" fill="none" stroke="currentColor"
+                                                        viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                                                        <path stroke-linecap="round" stroke-linejoin="round"
+                                                            stroke-width="2"
+                                                            d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z">
+                                                        </path>
+                                                    </svg>
+                                                    <span>View stats</span>
+                                                </a>
+                                            @endif
                                             @if ($post->isPublished())
                                                 <a href="{{ route('posts.show', ['name' => $post->blog->name, 'post' => $post->slug]) }}"
                                                     class="flex items-center px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900 poppins"
