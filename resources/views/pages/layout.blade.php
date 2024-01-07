f757cd44a8204e5e5d0dc74ce6575c194d8e7db0
Mohamed jinas <boringdragon98@gmail.com>
2024-01-07 08:04:54 +0500

    WIP

--- resources/views/pages/layout.blade.php
+++ resources/views/pages/layout.blade.php
@@ -73,6 +73,10 @@
                         class="ml-10 font-medium text-gray-500 hover:text-gray-900 transition duration-150 ease-in-out">
                         Guidelines
                     </a>
+                    <a href="/change-log" style="font-family: Poppins;"
+                    class="ml-10 font-medium text-gray-500 hover:text-gray-900 transition duration-150 ease-in-out">
+                    Change log
+                    </a>
                     <a href="https://write.mv/writemv/" style="font-family: Poppins;"
                         class="ml-10 font-medium text-gray-500 hover:text-gray-900 transition duration-150 ease-in-out">
                         Our blog
