084b159d5ea8ada4636d93b696099574f2aab074
Mohamed jinas <boringdragon98@gmail.com>
2024-01-23 02:21:00 +0500

    WIP

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,6 +6,7 @@ use App\Traits\BelongsToTeam;
 use Filament\Models\Contracts\FilamentUser;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -66,6 +67,11 @@ class User extends Authenticatable implements FilamentUser, MustVerifyEmail
     public function posts(): HasMany
     {
         return $this->hasMany(Post::class);
+    }
+
+    public function team(): BelongsTo
+    {
+        return $this->belongsTo(Team::class);
     }
 
     public function getActivitylogOptions(): LogOptions
