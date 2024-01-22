45a5def5f0517fba93c24a9a6a832a5b80b55874
Mohamed jinas <boringdragon98@gmail.com>
2024-01-23 02:35:51 +0500

    WIP

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,7 +6,6 @@ use App\Traits\BelongsToTeam;
 use Filament\Models\Contracts\FilamentUser;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
